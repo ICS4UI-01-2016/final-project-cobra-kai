01a075fa01cd8895f20c1dff2b21f170e4d2cfc9
moore3607 <moore3607@HRH-IC0027346.wrdsb.ca>
2017-01-10 13:30:18 -0500

    mine's better

---
+++ core/src/com/finalgame/game/KoopaBoi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.finalgame.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ *
+ * @author moore3607
+ */
+public class KoopaBoi {
+    
+    private Vector3 position;
+    private Vector3 velocity;
+    
+    
+    private final float GRAVITY = 15;
+    private final float MOVEMENT = 100;
+    
+    public KoopaBoi(int x, int y){
+        position = new Vector3(x,y,0);
+    }
+    
+}
